6788df9ade2d31b6ec6df181e5b257ffad2d7dfe
Anton Trapeznikov <ant.trapeznikov@yandex.ru>
2026-08-11 20:23:29 +0500

    Fix repo url

--- script_consent/__init__.py
+++ script_consent/__init__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("django-script-consent")
 except PackageNotFoundError:  # pragma: no cover - raw path / pre-install
-    __version__ = "0.2.0"
+    __version__ = "0.2.1"
